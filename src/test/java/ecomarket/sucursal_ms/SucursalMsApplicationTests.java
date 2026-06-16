3124f7368a1744e5f5b8a649f847922168811f21
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-06-16 19:49:19 -0400

    creacion microservicio sucursal

---
+++ src/test/java/ecomarket/sucursal_ms/SucursalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomarket.sucursal_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucursalMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
